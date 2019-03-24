7945f348ebc643bdd73d2102a2cedcf5002bdc66
EngelOfChipolata <buisanguilhem@gmail.com>
2019-03-24 21:30:17 +0100

    Adds detection of the black inside the puck to find the center of the puck

--- test_opencv_color.py
+++ test_opencv_color.py
@@ -51,6 +51,34 @@ for pt in puck_poses:
   cv2.line(img_poses, (pt[0]-5, pt[1]), (pt[0]+5, pt[1]), (0, 255, 0))
   cv2.line(img_poses, (pt[0], pt[1]-5), (pt[0], pt[1]+5), (0,255,0))
 
+
+for c in filtered_contours:
+  x, y, w, h = cv2.boundingRect(c)
+  cropped_hsv = hsv[y:y+h, x:x+w].copy()
+  cv2.imshow("cropped", cropped_hsv)
+  cropped_mask = 255 - cv2.inRange(cropped_hsv, (0, 0, 0), (255, 255, 90))
+  cv2.imshow("cropped_mask", cropped_mask)
+  kernel = np.ones((12,12),np.uint8)
+  cropped_closed = cv2.dilate(cropped_mask, kernel, iterations=1)
+  cv2.imshow("cropped_closed", cropped_closed)
+  cropped_contour = cropped_hsv.copy()
+  contours, h = cv2.findContours(cropped_closed, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+  center_contour = None
+  center_contour_m = {'m00':0}
+  for i, ci in enumerate(contours):
+    if h[0][i][2] != -1:
+      continue  # Looking for last in trees (should be looking for max depth...)
+    M = cv2.moments(ci)
+    if M['m00'] > center_contour_m['m00']:
+      center_contour = ci
+      center_contour_m = M
+  cv2.drawContours(cropped_contour, [center_contour], -1, (255, 0, 0), 3)
+  cv2.imshow("inside contour", cropped_contour)
+  center_pose = (x + int(center_contour_m['m10']/center_contour_m['m00']), y + int(center_contour_m['m01']/center_contour_m['m00']))
+  cv2.line(img_poses, (center_pose[0]-5, center_pose[1]), (center_pose[0]+5, center_pose[1]), (0, 0, 255))
+  cv2.line(img_poses, (center_pose[0], center_pose[1]-5), (center_pose[0], center_pose[1]+5), (0,0,255))
+  
+
 cv2.imshow("plop", img2)
 cv2.imshow("plip", mask)
 cv2.imshow("plup", res)
